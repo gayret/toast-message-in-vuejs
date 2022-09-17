6ad0b3dc53c2799e53752c49c80b8f35638b362b
Safa Gayret <safa.gayret@tsoft.com.tr>
2022-09-17 23:19:04 +0300

    clear css

--- src/App.vue
+++ src/App.vue
@@ -28,18 +28,6 @@ const onInfoMessage = () => {
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-
 .buttons {
   display: flex;
   justify-content: center;
